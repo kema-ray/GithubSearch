772ca7691c4af8f6a439b7f31b77c12b9166be6c
kema-ray <rachelkemuma99@gmail.com>
2022-04-16 22:04:36 +0300

    append information to the date-count pipe

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -5,8 +5,18 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateCountPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
-  }
+  transform(value: any): number {
+    let today:Date = new Date();
+    let todayWithNoTime:any = new Date(today.getFullYear(), today.getMonth(),today.getDate())
+    var dateDifference = Math.abs(value - todayWithNoTime)
+    const SecondsInDay = 86400;
+    var dateDifferenceSeconds = dateDifference*0.001;
+    var dateCounter = dateDifference/SecondsInDay;
 
+    if (dateCounter >= 1 && value > todayWithNoTime){
+      return dateCounter;
+    }else{
+    return 0;
+  }
+  }
 }
